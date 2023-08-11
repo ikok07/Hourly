87b01b8b1107f947b12439376ffcd857a5872769
Kaloyan Stefanov <kokmarok@gmail.com>
2023-08-11 21:31:28 +0300

    Initial Commit

---
+++ Hourly/HourlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HourlyApp.swift
+//  Hourly
+//
+//  Created by Kaloyan Petkov on 11.08.23.
+//
+
+import SwiftUI
+
+@main
+struct HourlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

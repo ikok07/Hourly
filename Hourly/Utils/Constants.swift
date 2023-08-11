e30849ad310fb8cc95c6cc08b5e447a828ff46da
Kaloyan Stefanov <kokmarok@gmail.com>
2023-08-11 21:39:16 +0300

    Perfom initial setup of the application

---
+++ Hourly/Utils/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  Hourly
+//
+//  Created by Kaloyan Petkov on 11.08.23.
+//
+
+import SwiftUI
+
+struct K {
+    
+    //MARK: - Colors
+    static let bg = Color("bg")
+    static let cta = Color("cta")
+    static let white = Color("white")
+    static let secondary = Color("secondary")
+    
+}
